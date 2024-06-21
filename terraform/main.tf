a8285b76fa314df2ab314aadd435feb14cb80405
JustFiesta <mateusz.bocak9@gmail.com>
2024-06-21 10:30:13 +0200

    Compute variables added

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 module "network" {
-     source = "./modules/network"
+    source = "./modules/network"
 }
 
 /*
@@ -31,16 +31,20 @@ module "database" {
 
     sec_group_id = module.network.sec_group_id
     subnet_ids   = module.network.subnet_ids
+
+    depends_on   = [module.network]
 }
 
 module "compute" {
     source = "./modules/compute"
 
-    vpc_id       = module.network.vpc_id
-    public_sub_id   = module.network.public_sub_id
-    private_sub_id   = module.network.private_sub_id
-    ssh_sec_group = module.network.ssh_sec_group
-    http_sec_group = module.network.http_sec_group
+    ami_id            = "ami-0776c814353b4814d" # ubuntu 24.04 LTS AMI
+    vpc_id            = module.network.vpc_id
+    public_sub_id     = module.network.public_sub_id
+    private_sub_id    = module.network.private_sub_id
+    ssh_sec_group     = module.network.ssh_sec_group
+    http_sec_group    = module.network.http_sec_group
+    jeknins_sec_group = module.network.jeknins_sec_group
 
     depends_on   = [module.network]
 }
