858ebec92e975ef470a446d355f8c4ecd3a48dc5
JustFiesta <mateusz.bocak9@gmail.com>
2024-06-25 12:58:48 +0200

    Added second VM in the public_b  subnet

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,7 +1,16 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami-id
   instance_type = "t3.micro"
-  subnet_id     = var.public_sub_id
+  subnet_id     = var.public_sub_a_id
+  vpc_security_group_ids = [var.http_sec_group]
+
+  depends_on = [aws_instance.jenkins]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami-id
+  instance_type = "t3.micro"
+  subnet_id     = var.public_sub_b_id
   vpc_security_group_ids = [var.http_sec_group]
 
   depends_on = [aws_instance.jenkins]
@@ -10,6 +19,6 @@ resource "aws_instance" "app_server" {
 resource "aws_instance" "jenkins" {
   ami           = var.ami-id
   instance_type = "t3.micro"
-  subnet_id     = var.public_sub_id
+  subnet_id     = var.public_sub_a_id
   vpc_security_group_ids = [var.ssh_sec_group, var.jenkins_sec_group]
 }
