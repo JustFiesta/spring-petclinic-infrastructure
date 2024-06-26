259b0f01fc8e66ed60b5ca35306075ff6dc21179
JustFiesta <mateusz.bocak9@gmail.com>
2024-06-26 13:25:02 +0200

    RDS module

---
+++ terraform/modules/database/main.tf
@@ -0,0 +1,17 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = var_dbname
+  username             = var_username
+  password             = var_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  vpc_security_group_ids = [var.rds_sec_group]
+
+  tags = {
+    Name = "capstone_rds_mysql"
+  }
+}
