7996d7f9d7b873a88c69cd443bed1073b7ba0a4e
JustFiesta <mateusz.bocak9@gmail.com>
2024-06-26 14:05:55 +0200

    DB variable passes names fix

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -3,9 +3,9 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  db_name              = var.dbname
-  username             = var.username
-  password             = var.password
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   publicly_accessible  = false
