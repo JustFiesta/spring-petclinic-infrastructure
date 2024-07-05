425abe3df6c1dc09abd2f071b8e7d976f6c6f701
JustFiesta <mateusz.bocak9@gmail.com>
2024-07-05 20:00:23 +0200

    Added sample variable for user data in compute module, for providing agent on instance startup

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "app_server_a" {
   instance_type = var.app_instance_type
   subnet_id     = var.public_sub_a_id
   vpc_security_group_ids = [var.ssh_sec_group, var.http_sec_group]
-
+  user_data = base64encode(var.app_server_user_data)
+  
   key_name      = var.ssh_key_name
 
   depends_on = [aws_instance.jenkins]
